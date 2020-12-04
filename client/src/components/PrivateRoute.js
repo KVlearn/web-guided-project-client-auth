16ae3b539b852b91579bb7509e9876d7ec607218
kirkby <kirkby@neighbor.com>
2020-12-03 18:01:13 -0700

    lecture work-in-progress

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Route from 'react-router-dom';
 
-const PrivateRoute = { component: Component, ...props } => {
+const PrivateRoute = ({ component: Component, ...props }) => {
   return <Route component={Component} {...props} />
 }
 
