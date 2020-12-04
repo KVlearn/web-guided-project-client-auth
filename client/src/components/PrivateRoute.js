90d4cabf8fd47f8fd294c1dc21aa044408831c7c
kirkby <kirkby@neighbor.com>
2020-12-03 18:00:24 -0700

    lecture work-in-progress

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Route from 'react-router-dom';
 
-const PrivateRoute = props => {
-  return <Route {...props} />
+const PrivateRoute = { component: Component, ...props } => {
+  return <Route component={Component} {...props} />
 }
 
 export default PrivateRoute;
